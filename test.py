173bd60d32320241bad1c6a8d70c68d8a28bad6c
staccato007 <56469529+staccato007@users.noreply.github.com>
2019-10-12 16:51:52 +0800

    test.py
    test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+""" def function_name()
+    do sth  //必须缩进 
+    return sth //可以有返回值，也可以没有 """
+
+def function_name(x,y,z):
+    x = x + 1
+    y[1] = y[1] + 1
+    z = [5,4,3,2,1]
+a = 5
+b = [1,2,3,4,5]
+c = [1,1,1,1,1]
+function_name(a,b,c)
+print('a ==',a)
+print('b ==',b)
+print('c ==',c)
